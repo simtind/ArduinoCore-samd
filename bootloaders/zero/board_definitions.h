99e69433a303d495bcc29bef4f4002f73e1780f8
Simen August Tinderholt <simtind@gmail.com>
2026-07-06 22:49:09 +0200

    Fix build issue and upload artifacts

--- bootloaders/zero/board_definitions.h
+++ bootloaders/zero/board_definitions.h
@@ -39,6 +39,8 @@
   #include "board_definitions_arduino_mkrnb1500.h"
 #elif defined(BOARD_ID_arduino_nano_33_iot)
   #include "board_definitions_arduino_nano_33_iot.h"
+#elif defined(BOARD_ID_simtind_lora_sense)
+  #include "board_definitions_simtind_lora_sense.h"
 #else
   #error You must define a BOARD_ID and add the corresponding definitions in board_definitions.h
 #endif
